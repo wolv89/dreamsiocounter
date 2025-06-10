b7f2d24354b8668f586b8b94bfef246cd4e710b0
Pete <peter.delafosse@gmail.com>
2025-06-10 22:53:38 +1000

    Handle missing file

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 
-	data, _ := os.ReadFile("./words.txt")
+	filename := "./words.txt"
+
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatalln("failed to read file:", err)
+	}
 
 	fmt.Println(CountWords(data))
 
