080f4c0d65f219382ca34e79342b33ecbddf87fa
Pete <peter.delafosse@gmail.com>
2025-06-11 21:28:27 +1000

    Manually handling runes

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"unicode"
+	"unicode/utf8"
 )
 
 func main() {
@@ -29,8 +30,9 @@ func CountWordsInFile(file *os.File) int {
 	wordCount := 0
 	isInsideWord := false
 
-	const bufferSize = 8192
+	const bufferSize = 4
 	buffer := make([]byte, bufferSize)
+	leftover := []byte{}
 
 	for {
 		size, err := file.Read(buffer)
@@ -38,18 +40,27 @@ func CountWordsInFile(file *os.File) int {
 			break
 		}
 
-		isInsideWord = !unicode.IsSpace(rune(buffer[0])) && isInsideWord
+		subBuffer := append(leftover, buffer[:size]...)
 
-		bufferCount := CountWords(buffer[:size])
-		if isInsideWord {
-			bufferCount--
+		for len(subBuffer) > 0 {
+
+			r, rsize := utf8.DecodeRune(subBuffer)
+			if r == utf8.RuneError {
+				break
+			}
+			subBuffer = subBuffer[rsize:]
+
+			if !unicode.IsSpace(r) && !isInsideWord {
+				wordCount++
+			}
+
+			isInsideWord = !unicode.IsSpace(r)
+
 		}
 
-		if !unicode.IsSpace(rune(buffer[size-1])) {
-			isInsideWord = true
-		}
+		leftover = leftover[:0]
+		leftover = append(leftover, subBuffer...)
 
-		wordCount += bufferCount
 	}
 
 	return wordCount
